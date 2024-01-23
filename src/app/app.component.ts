6fece00c54166b2884c34ea6944c417b562dba8f
joebarteluce <joe.barteluce@gmail.com>
2024-01-23 13:11:41 -0800

    updated relevant files to display logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,12 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>It's easy being br3ezy!</h1>
+    <img src="../assets/br3ezy_logo_banner.png" alt="br3ezy logo">
+    <p>Welcome to the br3ezy front end. It's good to have you. </p>
+  ` ,
+  styles: ['h1 { font-weight: normal; }']
 })
 export class AppComponent {
   title = 'test';
