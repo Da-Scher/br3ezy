0a9bcf8b355b071a00430011fc07c71f9efee645
joebarteluce <joe.barteluce@gmail.com>
2024-02-03 16:47:54 -0800

    add ng component gallery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-gallery',
+  standalone: true,
+  imports: [],
+  templateUrl: './gallery.component.html',
+  styleUrl: './gallery.component.css'
+})
+export class GalleryComponent {
+
+}
