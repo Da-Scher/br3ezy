8da540005ca2039dc4e02b1ad44215d2efe3bc69
joebarteluce <joe.barteluce@gmail.com>
2024-02-03 17:05:11 -0800

    Add ng component stream-listing

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StreamListingComponent } from '../stream-listing/stream-listing.component';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, StreamListingComponent],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
---
+++ src/app/stream-listing/stream-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stream-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './stream-listing.component.html',
+  styleUrl: './stream-listing.component.css'
+})
+export class StreamListingComponent {
+
+}
