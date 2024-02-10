21b70a8f5092bc41e6f821e988c5c370874a396f
Dak-Scher <dak.scher@proton.me>
2024-02-09 20:28:29 -0800

    server recieves srt connection

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ try {
 }
 const fs    = require('node:fs');
 const express = require('express');
+const {SRTReadStream} = require('@eyevinn/srt');
+
+// todo: replace me with a video player.
+const output = fs.createWriteStream('./output');
+
 const app = express();
+
 const path = require('path');
+
 app.use(express.static('dist/br3ezy/browser'));
 app.get("/", (req, res) => {
   res.sendFile('/dist/br3ezy/browser/index.html');
 });
-
 
 // todo: get key and cert later. also get more professional sounding names.
 const options = {
@@ -26,3 +32,9 @@ https.createServer(options, app)
   .listen(8000, (req, res) => {
     console.log("Server running at https://localhost:8000");
   });
+
+const host = new SRTReadStream('0.0.0.0', 2000);
+host.listen(readStream=> {
+  console.log('host(?) connected.');
+  readStream.pipe(output);
+});
