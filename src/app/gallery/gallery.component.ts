fcd50f8f4555cc2d07d5a2831f24ee7ca49ffd0d
joebarteluce <joe.barteluce@gmail.com>
2024-02-24 17:41:06 -0800

    Add local search functionality

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -14,13 +14,28 @@ import { ListingService } from '../listing.service'; // this service delivers th
 
 export class GalleryComponent {
 
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa'; // generic crap from the tutorial
-
-  streamListingList: StreamListing[] = []; // let there be an array which is hollow and empty
+  streamListingList: StreamListing[] = []; // Holds a list of Stream Listings
+  filteredListingList: StreamListing[] = []; // Holds a filtered list of Stream Listings
   listingService: ListingService = inject(ListingService); // inject data goodness from the listing service
 
+
   constructor() {
-    this.streamListingList = this.listingService.getAllStreamListings(); // Call function from listing service to get that data
+    this.streamListingList = this.listingService.getAllStreamListings(); // Get all the listings and put them in the array
+    this.filteredListingList = this.streamListingList; // start off the filtered list mirroring the array
   }
+
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredListingList = this.streamListingList;
+      return;
+    }
+
+    this.filteredListingList = this.streamListingList.filter(
+      streamListing => streamListing?.title.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
+
 
 } // end class GalleryComponent
