e288efa5a0c66248a6ecc875633b8bb42a64ee29
joebarteluce <joe.barteluce@gmail.com>
2024-02-25 11:05:43 -0800

    Remove static array of sample stream listings

--- src/app/listing.service.ts
+++ src/app/listing.service.ts
@@ -9,88 +9,17 @@ export class ListingService {
 
   constructor(private http: HttpClient) { }
 
-  // search API url
+  // stream API url
   url = 'https://localhost:8000/api/streams';
-
-
-  streamListingList: StreamListing[] = [
-    {
-      id: 0,
-      user_id: 0,
-      title: 'Linus\'s Stream',
-      description: 'A peak into the life of Linus, creator of the Linux kernel.',
-      start_time: 0,
-      is_active: true,
-      photo: 'https://ofa.guru/wp-content/uploads/2020/11/linus-torvalds-guided-tour-of-his-home-office-youtube-thumbnail.jpg'
-    },
-    {
-
-      id: 1,
-      user_id: 1,
-      title: 'Cthulu\'s Crib',
-      description: 'Cthulu shows off his South Pacific Ocean Crib / Prison.',
-      start_time: 0,
-      is_active: true,
-      photo: 'https://cdna.artstation.com/p/assets/images/images/052/142/696/large/jack-house-of-cthulu.jpg'
-
-    },
-    {
-
-      id: 2,
-      user_id: 2,
-      title: 'Tom\'s Green Thumb',
-      description: 'Tom Bombadil sharing his thoughts on sustainable gardening in the Old Forest',
-      start_time: 0,
-      is_active: true,
-      photo: 'http://www.brickbuilt.org/wp-content/uploads/2016/10/Featured-10-16.jpg'
-
-
-    },
-    {
-
-      id: 3,
-      user_id: 3,
-      title: 'Tour WSUV',
-      description: 'Explore the WSUV campus',
-      start_time: 0,
-      is_active: true,
-      photo: 'https://globalresearchsyndicate.com/wp-content/uploads/2020/05/WSUV-COVID-Research-Clark-County-Today-Copy-of-WSU-Vancouver-Campus-3-11-20-01.jpg'
-
-    },
-    {
-
-      id: 4,
-      user_id: 4,
-      title: 'Ninja Plays Fortnite',
-      description: 'Doing the things Ninja does! ',
-      start_time: 0,
-      is_active: true,
-      photo: 'https://images.firstpost.com/wp-content/uploads/2019/08/ninja-streamer.jpg'
-
-    },
-    {
-      id: 5,
-      user_id: 5,
-      title: 'Pokimane\'s New Home: Br3ezy',
-      description: 'Pokimane settling in to her new digs at Br3ezy after ending her contract with Twitch. ',
-      start_time: 0,
-      is_active: true,
-      photo: 'https://dotesports.com/wp-content/uploads/2022/06/20005308/Feature-Image-18.jpg'
-
-    },
-  ]
-
 
   async getAllStreamListings(): Promise<StreamListing[]> { // get all streams by searching with an empty keyword
     const data = await fetch(`${this.url}/search?keyword=`);
     return await data.json() ?? [];
-    // return this.streamListingList;
   }
 
   async getStreamListingById(id: number) : Promise<StreamListing | undefined> { // get a single stream listing by its id
     console.log("Listing service request stream id: ", id);
      const data = await fetch(`${this.url}/get/${id}`);
-    //const data = await fetch(`https://localhost:8000/api/streams/get/1`);
     return await data.json() ?? {};
   }
 
