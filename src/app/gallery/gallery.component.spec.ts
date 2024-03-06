7a11a1f6ba85945202251c836ecd24092a3c8c42
joebarteluce <joe.barteluce@gmail.com>
2024-03-05 17:36:51 -0800

    Add tests for Gallery, Listing Service, Watch

--- src/app/gallery/gallery.component.spec.ts
+++ src/app/gallery/gallery.component.spec.ts
@@ -18,7 +18,30 @@ describe('GalleryComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // acceptance test
+  it('should fill stream array with multiple results when there is no text input', () => {
+    component.filterResults('');
+    expect(component.streamListingList.length).toBeGreaterThanOrEqual(1);
   });
+
+  // acceptance test
+  it('should fill stream array with nothing when the input is foobar', () => {
+    component.filterResults('foobar');
+    expect(component.streamListingList.length).toBe(0);
+  });
+
+  // acceptance test
+  it('should fill stream array with results that match the filter query', () => {
+    const testQuery = 'Test';
+    component.filterResults(testQuery);
+    expect(component.streamListingList[0].title).toContain(testQuery);
+  });
+
+   // integration test - Gallery Component and Stream Listing Component
+   it('should load the stream listing component', () => {
+    const streamComp = fixture.debugElement.nativeElement.querySelector('app-stream-listing');
+    expect(streamComp).toBeTruthy();
+  })
+
+
 });
--- src/app/listing.service.spec.ts
+++ src/app/listing.service.spec.ts
@@ -8,12 +8,51 @@ describe('ListingService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule],
+      providers: [ListingService]
     });
     service = TestBed.inject(ListingService);
   });
 
+  // Acceptance Test
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // Integration Test - Listing Service and Stream Listing Interface
+  it('should return a streamListing', async () => {
+    await service.getStreamListingById(1).then((streamListing) => {
+      expect(streamListing).toBeDefined();
+    });
+  });
+
+  // White Box Test
+  // Combined with other white box tests of this component,
+  // 100% function coverage
+  it('should return the streamListing with ID 1', async () => {
+    await service.getStreamListingById(1).then((streamListing) => {
+      if(streamListing)
+        expect(streamListing.id).toBe(1);
+    });
+  });
+
+    // White Box Test
+    // Combined with other white box tests of this component,
+    // 100% function coverage
+    it('should not return a streamListing', async () => {
+      await service.getStreamListingById(82345).then((streamListing) => {
+          expect(streamListing).toBeUndefined();
+      });
+    });
+
+    // White Box Test
+    // Combined with other white box tests of this component,
+    // 100% function coverage
+  it('should return an array of streamListings', async () => {
+    await service.getAllStreamListings().then((streamListing) => {
+      expect(streamListing).toBeDefined();
+    });
+  });
+
+
 });
