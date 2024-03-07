eee71aec0a8eeeb2def75d18bf45695be95d2fa3
joebarteluce <joe.barteluce@gmail.com>
2024-03-07 12:21:55 -0800

    Add stream listing URL interpolation to video component

--- src/app/videoplayer/videoplayer.component.ts
+++ src/app/videoplayer/videoplayer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-videoplayer',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './videoplayer.component.css'
 })
 export class VideoplayerComponent {
-
+  @Input() streamURL: string | undefined;
 }
