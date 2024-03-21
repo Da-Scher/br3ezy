11106b58e1d4fbd57decbea0735ebda84706aeee
justinsprecco <justin@sprecco.com>
2024-03-21 11:47:28 -0700

    add jwt.exp check

--- client/src/app/auth/auth.guard.ts
+++ client/src/app/auth/auth.guard.ts
@@ -1,13 +1,25 @@
 import { CanActivateFn } from "@angular/router";
 import { Router } from "@angular/router";
 import { inject } from "@angular/core";
+import { jwtDecode } from "jwt-decode";
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const token = localStorage.getItem("token");
+
   if (!token) {
     router.navigate(["/login"], { queryParams: { returnUrl: state.url } });
     return false;
   }
+
+  const decoded: any = jwtDecode(token);
+  const expiration = decoded.exp * 1000;
+  const current = new Date().getTime();
+
+  if (current >= expiration) {
+    router.navigate(["/login"], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+
   return true;
 };
